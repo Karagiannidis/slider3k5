ed0105f1cb0a133bac1ce2fcfaac162297e998cf
Karagiannidis <p.karagiannidhs@gmail.com>
2019-07-17 16:46:16 +0300

    fix pages

---
+++ docs/script.js
@@ -0,0 +1,16 @@
+createSlider3k5("slider-container-id", {
+    // slidesFile: "./slides.json", // must be specified
+    slidesFile: "https://raw.githubusercontent.com/Karagiannidis/slider3k5/master/slides.json", // must be specified
+    hasArrows: true, //default true;
+    hasDots: true, //default true;
+    autoplayMode: false, //default false
+    autoplaySpeed: 1000, //default 3000
+    autoplayDirection: 'rtl', //default ltr
+    animationStyle: 1, //default 0, max 5
+    moreText: 'READ MORE' //default 'Read more'
+});
+
+
+
+    
+
